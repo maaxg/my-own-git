adb42c262eae6b825ab7fdaff423116f729f1b17
maaxg <maxgps5@hotmail.com>
2022-08-22 14:56:43 -0300

    refactor(test): creating specific test files for classes

---
+++ __tests__/commit.test.ts
@@ -0,0 +1,9 @@
+import { Commit } from "../src/commit"
+
+describe('Commit', () => {
+  test('should create a commit properly', () => {
+    const commit = new Commit('message', null)
+    expect(commit.message).toEqual('message')
+    expect(commit.id).toEqual("6f9b9af3cd6e8b8a73c2cdced37fe9f59226e27d")
+  })
+})
--- __tests__/git.test.ts
+++ __tests__/git.test.ts
@@ -1,27 +1,13 @@
-import { Commit } from "../src/commit";
 import { Git } from "../src/git";
-import { Repository } from "../src/repository";
 
 describe("Git", () => {
-
-  test('should return repo name properly', () => {
-    const repo = new Repository('first-repo');
-    expect(repo.name).toEqual('first-repo')
-  })
-  test('should create a commit properly', () => {
-    const commit = new Commit('message')
-    expect(commit.message).toEqual('message')
-    expect(commit.id).toEqual("6f9b9af3cd6e8b8a73c2cdced37fe9f59226e27d")
-  })
-
-  test('git should implements commit and repo', () => {
+  test('git should create commit and repo', () => {
     const git = new Git();
-    const commit = git.createCommit('message');
     const repository = git.createRepository('second-repo');
+    const commit = git.createCommit('message');
     expect(commit.message).toEqual('message')
     expect(repository.name).toEqual('second-repo')
   })
-
   test('git should return commit history', () => {
     const git = new Git();
     const commit1 = git.createCommit('message');
